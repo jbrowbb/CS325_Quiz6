91291779757c7412814d9027ad31edf82bc8d8ee
Jada Brown <jbrowbb@siue.edu>
2024-03-01 19:36:01 -0600

    adding Polygon class

--- l.py
+++ l.py
@@ -40,3 +40,25 @@ class Triangle(Shape):
     def __init__(self, base, height):
         self.base = base
         self.height = height
+
+    def get_area(self):
+        return 0.5 * self.base * self.height
+    
+
+class Polygon(Shape):
+    def __init__(self, sides, lengths):
+        if len(sides) != len(lengths):
+            raise ValueError("Sides and lengths must have the same nuber of elements")
+        
+        self.sides = sides
+        self.lengths = lengths
+    
+    def get_area(self):
+        center = [sum(x) / len(x) for x in zip(*self.lengths)]      # calculates the coordinate of the center of the polygon base
+        triangles = []
+
+        for i in range(len(self.sides)):
+            next_i = (i + 1) % len(self.sides)
+            triangles.append(Triangle(self.lengths[i], center[i]- center[next_i]))
+        
+        return sum(t.get_area() for t in triangles)     # interate though the sides of the polygon and creates triangles to approximate its area
