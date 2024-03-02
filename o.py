dba527b7c3ea1c9eee8529d23536c8baf9e740c1
Jada Brown <jbrowbb@siue.edu>
2024-03-01 18:11:49 -0600

    adding square and rectangle class

--- o.py
+++ o.py
@@ -16,3 +16,20 @@ class Circle(Shape):
         return area
     
 
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+    
+    def get_area(self):
+        area = math.pow(self.side,2)
+        return area
+    
+
+class Rectangle(Shape):
+    def __init__ (self, length, width):
+        self.length = length
+        self.width = width
+    
+    def get_area(self):
+        area = self.length * self.width
+        return area
